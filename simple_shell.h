a23c9b15c8cd7b827876dedf1e150c16bd976c3c
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-05-19 23:42:33 +0300

    Update simple_shell.h

--- simple_shell.h
+++ simple_shell.h
@@ -2,6 +2,7 @@
 #define SIMPLE_SHELL_H
 
 #include <stdio.h>
+#include <limits.h>
 
 typedef struct data
 {
@@ -19,5 +20,6 @@ int _atoi(char *s);
 int exit_shell(data_shell *datash);
 int _strlen(const char *s);
 int _isdigit(const char *s);
+int get_error(data_shell *datash, int eval);
 
 #endif
