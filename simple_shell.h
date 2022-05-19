c203f9f362daefc6562fb2c08c68133d1cfb59e6
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-05-19 23:50:23 +0300

    Update simple_shell.h

--- simple_shell.h
+++ simple_shell.h
@@ -21,5 +21,9 @@ int exit_shell(data_shell *datash);
 int _strlen(const char *s);
 int _isdigit(const char *s);
 int get_error(data_shell *datash, int eval);
+void free_data(data_shell *datash);
+void set_data(data_shell *datash, char **av);
+int main(int ac, char **av);
+char *_strdup(const char *s);
 
 #endif
