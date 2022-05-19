946cb0cc6b4153f2747d149560073b30dbeed1fb
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-05-19 23:33:26 +0300

    Update simple_shell.h

--- simple_shell.h
+++ simple_shell.h
@@ -1,7 +1,11 @@
 #ifndef SIMPLE_SHELL_H
 #define SIMPLE_SHELL_H
 
+#include <stdio.h>
+
 int _atoi(char *s);
 int exit_shell(data_shell *datash);
+int _strlen(const char *s);
+int _isdigit(const char *s);
 
 #endif
